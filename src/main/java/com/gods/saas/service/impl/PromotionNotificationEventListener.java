490951a69003a66ae6b9700ad414f111bbb96447
Derwin Adrian Nieves Martinez <extdnieves@transbank.cl>
2026-08-24 18:35:27 -0500

    sunat electronica

--- src/main/java/com/gods/saas/service/impl/PromotionNotificationEventListener.java
+++ src/main/java/com/gods/saas/service/impl/PromotionNotificationEventListener.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -20,7 +21,7 @@ public class PromotionNotificationEventListener {
     private final NotificationService notificationService;
 
     @Async
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(PromotionNotificationRequestedEvent event) {
         if (event == null || event.promotionId() == null) return;
