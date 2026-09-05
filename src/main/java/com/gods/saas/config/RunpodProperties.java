eba223fd1647b9dee73a571100e1522ed183f524
Derwin Adrian Nieves Martinez <extdnieves@transbank.cl>
2026-09-05 18:31:05 -0500

    mejoras

--- src/main/java/com/gods/saas/config/RunpodProperties.java
+++ src/main/java/com/gods/saas/config/RunpodProperties.java
@@ -3,11 +3,9 @@ package com.gods.saas.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Configuration
 @ConfigurationProperties(prefix = "runpod")
 public class RunpodProperties {
 
